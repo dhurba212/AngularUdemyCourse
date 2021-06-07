82ca7b272ed338884dc672cf7776207d230f0cb3
dhurba <dhurbag21@gmail.com>
2021-06-07 11:44:03 -0400

    created folders for each project

--- TranslationService/src/app/app.component.ts
+++ TranslationService/src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LanguageTranslationApp';
   text:string;
   translatedText:string;
 
