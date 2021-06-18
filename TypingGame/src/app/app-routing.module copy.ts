8e34731e076ef8b754dbaf7038941db4b93b085c
dhurba <dhurbag21@gmail.com>
2021-06-17 21:09:08 -0400

    new project from udemy Typing Game

---
+++ TypingGame/src/app/app-routing.module copy.ts
@@ -0,0 +1,19 @@
+
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const routes: Routes = [
+  {path:'',component:AppComponent},
+  {path:'dashboard',component:AppComponent},
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
