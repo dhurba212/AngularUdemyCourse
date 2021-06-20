b56916d48168bfda859e33b3a35dee6d06360b36
dhurba <dhurbag21@gmail.com>
2021-06-19 21:37:37 -0400

    completed

--- TypingGame/src/app/app.component.ts
+++ TypingGame/src/app/app.component.ts
@@ -8,7 +8,26 @@ import { lorem } from 'faker';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  lorem:string=lorem.sentences();
+  randomText:string=lorem.text();
+  typedByUser:string='';
+  
+
+  onInput(value:string){
+    this.typedByUser=value;
+    // for(let i=0;i<value.length;i++){
+    //   if(this.randomText.charAt(i)===value.charAt(i)){
+    //     this.randomText.replace(this.randomText.charAt(i),'<span style="color: red;">this.randomText.charAt(i)</span>')
+    //   }
+    //   }
+    }
+    compare(randomLetter:string,typedByUserLetter:string){
+      if(!typedByUserLetter){
+        return "pending"
+      }
+      return randomLetter===typedByUserLetter? 'correct':'incorrect'
+    }
+    
+
 
 }
 
